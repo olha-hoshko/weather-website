5a826aaf152da5ec1f58f7f91d5ede76e2207ee2
olha.hoshko <olha.hoshko.w@gmail.com>
2023-03-22 16:56:12 +0200

    setup gh-pages

--- src/components/today-forecast/component.tsx
+++ src/components/today-forecast/component.tsx
@@ -1,13 +1,37 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useLayoutEffect, useState } from "react";
 import { useWeatherForecast } from "../../features/weather-forecast";
 import { v4 as uuidv4 } from 'uuid';
 import { TodayForecastData } from "../today-forecast-data";
 
+const hoursDesktop = ['06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
+const hoursMobile = ['09:00', '15:00', '21:00'];
+
+const useWindowSize = () => {
+  const [width, setWidth] = useState<number>(0);
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return width;
+}
+
 export const TodayForecast: FC = () => {
   const { weatherForecastData } = useWeatherForecast();
   const [todayForecastData, setTodayForecastData] = useState<any>({ hour: [] });
+  const windowWidth = useWindowSize();
 
-  const hoursToShow = ['06:00', '09:00', '12:00', '15:00', '18:00', '21:00'];
+  const getHoursToShow = () => {
+    if (windowWidth > 768) {
+      return hoursDesktop;
+    }
+    return hoursMobile;
+  }
+
+  const hoursToShow = getHoursToShow();
 
   useEffect(() => {
     if (Object.keys(weatherForecastData.forecast).length === 0) {
