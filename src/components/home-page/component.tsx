655a07090ec4105174c3dadaa9144d0f1c51a152
olha.hoshko <olha.hoshko.w@gmail.com>
2023-03-24 16:10:58 +0200

    change styles of a home page

--- src/components/home-page/component.tsx
+++ src/components/home-page/component.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { fetchAsyncWeatherForecast, useWeatherForecast } from "../../features/weather-forecast";
+import { ThemeChangeButton } from "../theme-change-button";
 
 export const HomePage: FC = () => {
   const { dispatch } = useWeatherForecast();
@@ -29,19 +30,22 @@ export const HomePage: FC = () => {
   return (
     <>
       <div className='home-page'>
-        <div className='main-title-container'>
-          <h3>What city are you looking for?</h3>
-        </div>
-        <div className='search-city-container'>
-          <AsyncPaginate id='search-input'
-            placeholder='Search the city...'
-            debounceTimeout={600}
-            value={search}
-            onChange={handleChange}
-            loadOptions={loadOptions}
-            getOptionLabel={(option: any) => `${option.name}, ${option.country}`}
-            getOptionValue={(option: any) => `${option.lat} ${option.lon}`}
-          />
+        <ThemeChangeButton />
+        <div className='home-page-main'>
+          <div className='main-title-container'>
+            <h3>What city are you looking for?</h3>
+          </div>
+          <div className='search-city-container'>
+            <AsyncPaginate id='search-input'
+              placeholder='Search the city...'
+              debounceTimeout={600}
+              value={search}
+              onChange={handleChange}
+              loadOptions={loadOptions}
+              getOptionLabel={(option: any) => `${option.name}, ${option.country}`}
+              getOptionValue={(option: any) => `${option.lat} ${option.lon}`}
+            />
+          </div>
         </div>
       </div>
     </>
