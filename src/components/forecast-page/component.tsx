0e8a6be949419d05828d50677e7c378053fdf613
olha.hoshko <olha.hoshko.w@gmail.com>
2023-03-24 16:14:02 +0200

    changed main page transition

--- src/components/forecast-page/component.tsx
+++ src/components/forecast-page/component.tsx
@@ -24,7 +24,7 @@ export const ForecastPage: FC = () => {
           : <m.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 0.7, ease: 'easeInOut' }} >
+            transition={{ duration: 0.8, ease: 'easeInOut' }} >
             <div className="App">
               <ThemeChangeButton />
               <div className='main-container'>
