9536658b9a9b736291215f0612c5421ab966a33a
olha.hoshko <olha.hoshko.w@gmail.com>
2023-03-28 15:15:12 +0300

    fixed change theme button, when going from home page to main page in a dark theme mode.

--- src/components/theme-change-button/component.tsx
+++ src/components/theme-change-button/component.tsx
@@ -1,13 +1,22 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
+import { ThemeColors } from '../../enums/enums';
 import { useThemeColor } from '../../features/theme-color/themeColorSlice';
 import { DarkThemeIcon, LightThemeIcon } from '../theme-icons';
 
 export const ThemeChangeButton: FC = () => {
   const { changeThemeColor, themeColor } = useThemeColor();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if(!inputRef.current) return;
+    if(themeColor === ThemeColors.dark) {
+      inputRef.current.checked = true;
+    }
+  }, [themeColor]);
 
   return (
     <div className='theme-change-button-container'>
-      <input type='checkbox' id='toggle-dark' onClick={changeThemeColor} />
+      <input ref={inputRef} type='checkbox' id='toggle-dark' onClick={changeThemeColor} />
       <label htmlFor='toggle-dark' id='toggle-label'>
         <LightThemeIcon currentTheme={themeColor} />
         <DarkThemeIcon currentTheme={themeColor} />
